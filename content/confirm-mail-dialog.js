5969aa734c2bd295653219108bab9dff759d5385
YUKI Hiroshi <shimoda@clear-code.com>
2017-10-27 18:56:29 +0900

    Reconfirm based on config correctly

--- content/confirm-mail-dialog.js
+++ content/confirm-mail-dialog.js
@@ -492,7 +492,7 @@ var ConfirmMailDialog = {
 	},
 
 	reconfirmForExceptionalOtherDomains: function () {
-		return this.prefs.getPref(CA_CONST.EXCEPTIONAL_DOMAINS_ONLY_WITH_ATTACHMENT) &&
+		return !this.prefs.getPref(CA_CONST.EXCEPTIONAL_DOMAINS_ONLY_WITH_ATTACHMENT) ||
 				AttachmentManager.hasAttachments();
 	},
 
