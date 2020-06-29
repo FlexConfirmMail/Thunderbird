6202db1d99da6fe26a22e763f1d1527636190489
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-06-29 18:28:32 +0900

    Define default configs

--- webextensions/common/common.js
+++ webextensions/common/common.js
@@ -9,6 +9,51 @@ import Configs from '/extlib/Configs.js';
 import * as Constants from './constants.js';
 
 export const configs = new Configs({
+  confirmationMode: Constants.CONFIRMATION_MODE_ALWAYS,
+  internalDomains: [],
+
+  exceptionalDomainsHighlightMode: Constants.EXCEPTIONAL_HIGHLIGHT_MODE_ALWAYS,
+  exceptionalDomainsConfirmationMode: Constants.EXCEPTIONAL_CONFIRMATION_MODE_ALWAYS,
+  exceptionalDomains: [],
+  exceptionalDomainsSource: Constants.SOURCE_CONFIG,
+  exceptionalDomainsFile: '',
+
+  exceptionalSuffixesConfirm: true,
+  exceptionalSuffixes: [],
+  exceptionalSuffixesSource: Constants.SOURCE_CONFIG,
+  exceptionalSuffixesFile: '',
+
+  confirmOnlyInternals: true,
+  confirmMultipleRecipientDomains: false,
+  minRecipientsCount: 0,
+  overrideDelay: 0,
+  overrideDelayTimes: 0,
+
+  exceptionalDomainDialogTitle: '',
+  exceptionalDomainDialogMessage: '',
+  exceptionalSuffixDialogTitle: '',
+  exceptionalSuffixDialogMessage: '',
+  confirmMultipleRecipientDomainsDialogTitle: '',
+  confirmMultipleRecipientDomainsDialogMessage: '',
+
+  allowCheckAllInternalDomains: true,
+  allowCheckAllOtherDomains: false,
+  allowCheckAllFileNames: false,
+  requireCheckSubject: false,
+  requireCheckBody: false,
+  requireReinputAttachmentNames: false,
+  highlightUnmatchedDomains: false,
+  largeFontSizeForAddresses: false,
+  alwaysLargeDialog: false,
+  alwaysLargeDialogMinWidth: 680,
+  emphasizeTopMessage: false,
+  topMessage: '',
+  emphasizeRecipientType: false,
+
+  showCountdown: false,
+  countdownTime: 5,
+  countdownAllowSkip: true,
+
   configsVersion: 0,
   debug: false
 }, {
