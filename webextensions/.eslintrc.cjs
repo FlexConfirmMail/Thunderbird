26230f8266c4c93cd0512e60800e04a877e4e8f4
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-06-29 18:04:43 +0900

    Prepare WebExtensions version

---
+++ webextensions/.eslintrc.cjs
@@ -0,0 +1,49 @@
+/*eslint-env commonjs*/
+/*eslint quote-props: ['error', "always"] */
+
+module.exports = {
+  'root': true,
+
+  // https://qiita.com/kurkuru/items/d4eebd34f0898c6a2d5a
+  // this is required to accept `import.meta.url`
+  'parser': 'babel-eslint',
+
+  'parserOptions': {
+    'ecmaVersion': 2017,
+  },
+
+  'env': {
+    'browser': true,
+    'es6': true,
+    'webextensions': true,
+    'node': true,
+  },
+
+  'settings': {
+    'import/resolver': {
+      'babel-module': {
+        'root': ['./'],
+      }
+    }
+  },
+
+  'rules': {
+    // stylisitc problem
+    'indent': ['warn', 2, {
+      'SwitchCase': 1,
+      'MemberExpression': 1,
+      'CallExpression': {
+        'arguments': 'first',
+      },
+      'VariableDeclarator': {
+        'var': 2,
+        'let': 2,
+        'const': 3,
+      }
+    }],
+    'quotes': ['warn', 'single', {
+      'avoidEscape': true,
+      'allowTemplateLiterals': true,
+    }],
+  }
+};
