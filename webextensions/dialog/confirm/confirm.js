ef523589fe5c98826279befe47facb3c4a89086f
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-07-10 14:27:34 +0900

    Hide disabled UIs

--- webextensions/dialog/confirm/confirm.js
+++ webextensions/dialog/confirm/confirm.js
@@ -15,8 +15,28 @@ import * as Dialog from '/common/dialog.js';
 
 let mParams;
 
+const mTopMessage          = document.querySelector('#top-message');
+const mInternalsAllCheck   = document.querySelector('#internalsAll');
+const mInternalsTable      = document.querySelector('#internals');
+const mExternalsAllCheck   = document.querySelector('#externalsAll');
+const mExternalsTable      = document.querySelector('#externals');
+const mSubjectCheck        = document.querySelector('#subject');
+const mBodyCheck           = document.querySelector('#body');
+const mBodyField           = document.querySelector('#bodyField');
+const mAttachmentsAllCheck = document.querySelector('#attachmentsAll');
+const mAttachmentsTable    = document.querySelector('#attachments');
+
 configs.$loaded.then(async () => {
   mParams = await Dialog.getParams();
+
+  mTopMessage.textContent = configs.topMessage;
+  mTopMessage.classList.toggle('hidden', !configs.topMessage);
+
+  mInternalsAllCheck.classList.toggle('hidden', !configs.allowCheckAllInternals);
+  mExternalsAllCheck.classList.toggle('hidden', !configs.allowCheckAllExternals);
+  mAttachmentsAllCheck.classList.toggle('hidden', !configs.allowCheckAllAttachments);
+  mSubjectCheck.closest('p').classList.toggle('hidden', !configs.requireCheckSubject);
+  mBodyCheck.closest('div').classList.toggle('hidden', !configs.requireCheckBody);
 
   Dialog.notifyReady();
 });
